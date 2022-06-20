64671def200860a08ab42b215ba12a0ad407c0bd
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2022-06-20 21:45:19 +0300

    controller

--- index.js
+++ index.js
@@ -1,96 +1,296 @@
 const http = require(`http`);
-const oracledb = require('oracledb');
 const fs = require('fs');
 const path = require('path');
 const basePath = path.join(__dirname, 'public');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+
 const PORT = process.env.PORT || 3000;
 
-require('dotenv').config();
-let connection;
-
-(async () => {
-  connection = await oracledb
-    .getConnection({
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      connectString: process.env.DB_HOST,
-    })
-    .then((conn) => {
-      console.log('Connected to database');
-      return conn;
-    })
-    .catch((err) => {
-      console.error(err.message);
-      return;
-    });
-})();
-
 const server = http.createServer((req, res) => {
-  // Routes for static files
-  if (req.url === '/') {
-    fs.readFile(`${basePath}/index.html`, (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        return res.end('404 Not Found');
+  if (req.method === 'GET') {
+    // Decode the token
+    const decodedToken = parseToken(req, res);
+    // Home
+    if (req.url === '/') {
+      fs.readFile(`${basePath}/index.html`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    // Login
+    else if (req.url === '/login') {
+      fs.readFile(`${basePath}/pages/login.html`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+
+      // User preferences
+    }
+
+    // Register
+    else if (req.url === '/register') {
+      fs.readFile(`${basePath}/pages/register.html`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    // Contact
+    else if (req.url === '/contact') {
+      fs.readFile(`${basePath}/pages/contact.html`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    // Map
+    else if (req.url === '/map') {
+      fs.readFile(`${basePath}/pages/map.html`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    // Dashboard
+    else if (req.url === '/dashboard') {
+      if (decodedToken && decodedToken.role === 'admin') {
+        fs.readFile(`${basePath}/pages/dashboard.html`, (err, data) => {
+          if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end('404 Not Found');
+          }
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(data);
+          return res.end();
+        });
+      } else {
+        console.log(decodedToken);
+        console.log('Not authorized');
+        // Redirect to login page
+        res.writeHead(302, { Location: '/login' });
+        return res.end();
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    });
+    }
+
+    // Add event
+    else if (req.url === '/add-event') {
+      if (decodedToken && decodedToken.role === 'admin') {
+        fs.readFile(`${basePath}/pages/event.html`, (err, data) => {
+          if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end('404 Not Found');
+          }
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(data);
+          return res.end();
+        });
+      } else {
+        console.log(decodedToken);
+        console.log('Not authorized');
+        // Redirect to login page
+        res.writeHead(302, { Location: '/login' });
+        return res.end();
+      }
+    }
+
+    // User preferences
+    else if (req.url === '/account') {
+      if (decodedToken) {
+        fs.readFile(`${basePath}/pages/user.html`, (err, data) => {
+          if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end('404 Not Found');
+          }
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(data);
+          return res.end();
+        });
+      } else {
+        console.log(decodedToken);
+        console.log('Not authorized');
+        // Redirect to login page
+        res.writeHead(302, { Location: '/login' });
+        return res.end();
+      }
+    }
+
+    // If it is a js
+    if (req.url.match(/\.js$/)) {
+      fs.readFile(`${basePath}${req.url}`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    // If it is a css
+    if (req.url.match(/\.css$/)) {
+      fs.readFile(`${basePath}${req.url}`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    // If it is a image
+    if (req.url.match(/\.(jpg|png|gif|ico|PNG)$/)) {
+      fs.readFile(`${basePath}${req.url}`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          return res.end('404 Not Found');
+        }
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.write(data);
+        return res.end();
+      });
+    }
+
+    // // If it is a page
+    // if (req.url.match(/\.html$/)) {
+    //   fs.readFile(`${basePath}${req.url}`, (err, data) => {
+    //     if (err) {
+    //       res.writeHead(404, { 'Content-Type': 'text/html' });
+    //       return res.end('404 Not Found');
+    //     }
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write(data);
+    //     return res.end();
+    //   });
+    // }
   }
 
-  // If it is a js
-  if (req.url.match(/\.js$/)) {
-    fs.readFile(`${basePath}${req.url}`, (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        return res.end('404 Not Found');
-      }
-      res.writeHead(200, { 'Content-Type': 'text/javascript' });
-      res.write(data);
-      return res.end();
-    });
-  }
-
-  // If it is a css
-  if (req.url.match(/\.css$/)) {
-    fs.readFile(`${basePath}${req.url}`, (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        return res.end('404 Not Found');
-      }
-      res.writeHead(200, { 'Content-Type': 'text/css' });
-      res.write(data);
-      return res.end();
-    });
-  }
-
-  // If it is a image
-  if (req.url.match(/\.(jpg|png|gif|ico|PNG)$/)) {
-    fs.readFile(`${basePath}${req.url}`, (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        return res.end('404 Not Found');
-      }
-      res.writeHead(200, { 'Content-Type': 'image/png' });
-      res.write(data);
-      return res.end();
-    });
-  }
-
-  // If it is a page
-  if (req.url.match(/\.html$/)) {
-    fs.readFile(`${basePath}${req.url}`, (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        return res.end('404 Not Found');
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    });
+  // Handle POST requests
+  if (req.method === 'POST') {
+    let parsedBody;
+    let body = '';
+    req
+      .on('data', (data) => {
+        body += data;
+      })
+      .on('end', () => {
+        parsedBody = JSON.parse(body);
+        handlePost(req, res, parsedBody);
+      });
   }
 });
+
+// JWT
+const jwtKey = 'my_secret_key';
+
+const handlePost = async (req, res, parsedBody) => {
+  if (req.url === '/login') {
+    const { email, password } = parsedBody;
+
+    console.log(parsedBody);
+
+    // fetch post data
+    const data = await axios
+      .post(`http://127.0.0.1:4000/login/`, {
+        email,
+        password,
+      })
+      .then((response) => {
+        return response.data;
+      });
+
+    console.log(data);
+
+    if (data.status === 'no user found' || data.status === 'wrong password') {
+      res.writeHead(401, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify({ status: 'Invalid username or password' }));
+      return res.end();
+    }
+
+    const token = jwt.sign(
+      { username: data.username, email: data.email, role: 'admin' },
+      jwtKey,
+      {
+        expiresIn: '1h',
+      }
+    );
+    // Put the token in the cookie
+    // res.setHeader('Set-Cookie', `token=${token}; HttpOnly`);
+    res.setHeader('Set-Cookie', `token=${token};`);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ status: 'existent user' }));
+    return res.end();
+  }
+};
+
+const parseToken = (req, res, next) => {
+  // Get the token from the cookie
+  const token = parseCookies(req)?.token;
+  if (!token) {
+    return null;
+  }
+
+  // Verify the authenticity of the token
+  let payload;
+  try {
+    payload = jwt.verify(token, jwtKey);
+    console.log(payload);
+  } catch (e) {
+    if (e instanceof jwt.JsonWebTokenError) {
+      // if the error thrown is because the JWT is unauthorized
+      return null;
+    }
+    // otherwise, return a bad request error
+    return null;
+  }
+  return payload;
+};
+
+function parseCookies(request) {
+  const list = {};
+  const cookieHeader = request.headers?.cookie;
+  if (!cookieHeader) return list;
+
+  cookieHeader.split(`;`).forEach(function (cookie) {
+    let [name, ...rest] = cookie.split(`=`);
+    name = name?.trim();
+    if (!name) return;
+    const value = rest.join(`=`).trim();
+    if (!value) return;
+    list[name] = decodeURIComponent(value);
+  });
+
+  return list;
+}
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
--- public/js/UserAccountService.js
+++ public/js/UserAccountService.js
@@ -1,56 +1,57 @@
-const { parse } = require("querystring");
-const http = require("http");
-const bcrypt = require("bcrypt");
-const hostname = "127.0.0.1";
+const { parse } = require('querystring');
+const http = require('http');
+const bcrypt = require('bcrypt');
+const hostname = '127.0.0.1';
 const port = 4000;
 
 //Can be found in the Details page
 
 const server = http.createServer(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   const urlSearchParams = new URLSearchParams(req.url);
   const params = Object.fromEntries(urlSearchParams.entries());
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Request-Method", "*");
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     res.writeHead(200);
     res.end();
     return;
   }
 
-  if (req.method == "GET") {
-  } else if (req.method == "POST") {
-    if (req.url.includes("register")) {
-      var body = "";
-      req.on("data", function (data) {
+  if (req.method == 'GET') {
+  } else if (req.method == 'POST') {
+    if (req.url.includes('register')) {
+      var body = '';
+      req.on('data', function (data) {
         body += data;
       });
 
-      req.on("end", function () {
+      req.on('end', function () {
         console.log(body);
         body = JSON.parse(body);
 
         postUser(body, function (response) {
-          res.writeHead(200, { "Content-Type": "application/json" });
+          res.writeHead(200, { 'Content-Type': 'application/json' });
           res.end(`${JSON.stringify(response)}`);
         });
       });
-    } else if (req.url.includes("login")) {
-      var body = "";
-      req.on("data", function (data) {
+    } else if (req.url.includes('login')) {
+      var body = '';
+      req.on('data', function (data) {
         body += data;
       });
 
-      req.on("end", function () {
+      req.on('end', function () {
         body = JSON.parse(body);
+        console.log(body);
 
         checkUser(body, function (response) {
-          res.writeHead(200, { "Content-Type": "application/json" });
+          res.writeHead(200, { 'Content-Type': 'application/json' });
           res.end(`${JSON.stringify(response)}`);
         });
       });
@@ -60,17 +61,17 @@ const server = http.createServer(async (req, res) => {
 
 function checkUser(body, callback) {
   let extractedMail = body.email;
-  let username = ""; //username-ul il extrag in DB
+  let username = ''; //username-ul il extrag in DB
   let extractedPassword = body.password;
-  let response = "";
+  let response = '';
 
-  const { Pool } = require("pg");
+  const { Pool } = require('pg');
   //const connectionString = 'postgres://ennfzieu:km1vCgMmJ3E__AlpbWFf7ueZuVh-lT8_@abul.db.elephantsql.com/ennfzieu'
   const pool = new Pool({
     //connectionString,
-    user: "postgres",
-    host: "164.92.194.239",
-    database: "postgres",
+    user: 'postgres',
+    host: '164.92.194.239',
+    database: 'postgres',
     port: 5432,
   });
   (async () => {
@@ -83,22 +84,34 @@ function checkUser(body, callback) {
       });
 
       const result = await client.query({
-        text: `SELECT PASSWORD FROM USERS WHERE EMAIL=$1`,
+        text: `SELECT * FROM USERS WHERE EMAIL=$1`,
         values: [extractedMail],
       });
-      console.log(bcrypt.compareSync(body.password, result.rows[0].password));
-      if (bcrypt.compareSync(body.password, result.rows[0].password)) {
-        console.log(check.rows);
-        response = {
-          status: "existent user",
-        };
+
+      if (result.rowCount == 1) {
+        console.log(body.password);
+        console.log(result.rows[0].password);
+        console.log(bcrypt.compareSync(body.password, result.rows[0].password));
+        if (bcrypt.compareSync(body.password, result.rows[0].password)) {
+          console.log(check.rows);
+          response = {
+            status: 'existent user',
+            username: result.rows[0].username,
+            email: result.rows[0].email,
+            role: result.rows[0].role,
+          };
+        } else {
+          response = {
+            status: 'wrong password',
+          };
+        }
       } else {
         console.log(
-          "user doesn't exist:" + extractedMail + " " + extractedPassword
+          "user doesn't exist:" + extractedMail + ' ' + extractedPassword
         );
 
         response = {
-          status: "no user found",
+          status: 'no user found',
         };
       }
       return callback(response);
@@ -115,48 +128,48 @@ function postUser(body, callback) {
   let extractedMail = body.email;
   let extractedUsername = body.username;
   let extractedPassword = body.password;
-  let response = "";
+  let response = '';
 
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(extractedPassword, salt);
 
-  const { Pool } = require("pg");
+  const { Pool } = require('pg');
   //const connectionString = 'postgres://ennfzieu:km1vCgMmJ3E__AlpbWFf7ueZuVh-lT8_@abul.db.elephantsql.com/ennfzieu'
   const pool = new Pool({
     //connectionString,
-    user: "postgres",
-    host: "164.92.194.239",
-    database: "postgres",
+    user: 'postgres',
+    host: '164.92.194.239',
+    database: 'postgres',
     port: 5432,
   });
   (async () => {
     const client = await pool.connect();
     try {
       const check = await client.query({
-        rowMode: "array",
+        rowMode: 'array',
         text: `SELECT * FROM USERS WHERE EMAIL=$1`,
         values: [extractedMail],
       });
 
       if (check.rowCount == 0) {
         const dim = await client.query({
-          rowMode: "array",
-          text: "SELECT * FROM USERS",
+          rowMode: 'array',
+          text: 'SELECT * FROM USERS',
         });
         const res = await client.query({
-          rowMode: "array",
+          rowMode: 'array',
           text: `INSERT INTO USERS VALUES($1,$2,$3,$4)`,
           values: [dim.rowCount + 1, extractedMail, extractedUsername, hash],
         });
 
         response = {
-          status: "new user",
+          status: 'new user',
         };
       } else {
-        console.log("email already exists:" + extractedMail);
+        console.log('email already exists:' + extractedMail);
 
         response = {
-          status: "user found",
+          status: 'user found',
         };
       }
       return callback(response);
@@ -170,21 +183,21 @@ function postUser(body, callback) {
 }
 
 function getUsers(callback) {
-  const { Pool } = require("pg");
+  const { Pool } = require('pg');
   //const connectionString = 'postgres://ennfzieu:km1vCgMmJ3E__AlpbWFf7ueZuVh-lT8_@abul.db.elephantsql.com/ennfzieu'
   const pool = new Pool({
     //connectionString,
-    user: "postgres",
-    host: "164.92.194.239",
-    database: "postgres",
+    user: 'postgres',
+    host: '164.92.194.239',
+    database: 'postgres',
     port: 5432,
   });
   (async () => {
     const client = await pool.connect();
     try {
       const res = await client.query({
-        rowMode: "array",
-        text: "SELECT * FROM USERS",
+        rowMode: 'array',
+        text: 'SELECT * FROM USERS',
       });
       console.log(res.rows);
       return callback(res.rows);
@@ -197,7 +210,7 @@ function getUsers(callback) {
 }
 
 function mergeGEOJSON(links, callback) {
-  var geojsonMerge = require("@mapbox/geojson-merge");
+  var geojsonMerge = require('@mapbox/geojson-merge');
 
   var mergedGeoJSON = geojsonMerge.merge(links[0], links[1]);
 
