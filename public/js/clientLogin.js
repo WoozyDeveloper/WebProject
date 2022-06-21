2235c90e45fe260437ae6154dfbf2d807a5b1462
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-06-22 00:01:21 +0300

    last update

--- public/js/clientLogin.js
+++ public/js/clientLogin.js
@@ -1,41 +1,49 @@
-// window.onload = function LoadWindow(event) {
-//   if (localStorage.getItem("sharedemail")) {
-//     window.location.href = "http://localhost:3000/pages/user.html";
-//   }
-// };
+function escapeHtml(text) {
+  var map = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#039;",
+  };
+
+  return String(text).replace(/[&<>"']/g, function (m) {
+    return map[m];
+  });
+}
 
 document.forms[0].onsubmit = async (e) => {
   e.preventDefault();
   const data = {
-    email: document.getElementById('email').value,
-    password: document.getElementById('password').value,
+    email: escapeHtml(document.getElementById("email").value),
+    password: escapeHtml(document.getElementById("password").value),
   }; //get data from form
 
   console.log(data);
   const settings = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: JSON.stringify(data),
   };
   try {
-    const fetchResponse = await fetch('http://localhost:3000/login', settings);
+    const fetchResponse = await fetch("http://localhost:3000/login", settings);
     const response = await fetchResponse.json();
     console.log(response);
 
-    if (response.status === 'existent user') {
+    if (response.status === "existent user") {
       //if the user exists(email and password match with those in the db)
-      localStorage.setItem('sharedemail', data.email);
-      window.location.href = '/account';
+      localStorage.setItem("sharedemail", data.email);
+      window.location.href = "/account";
     } else {
       //the user doesn't exist
-      const para = document.createElement('p');
+      const para = document.createElement("p");
       const node = document.createTextNode(
-        'Sorry the credentials you are using are invalid'
+        "Sorry the credentials you are using are invalid"
       );
       para.appendChild(node);
 
-      const element = document.getElementById('box');
-      console.log('element= ' + element);
+      const element = document.getElementById("box");
+      console.log("element= " + element);
       element.appendChild(para);
     }
   } catch (err) {
--- public/js/clientRegister.js
+++ public/js/clientRegister.js
@@ -1,43 +1,57 @@
+function escapeHtml(text) {
+  var map = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#039;",
+  };
+
+  return String(text).replace(/[&<>"']/g, function (m) {
+    return map[m];
+  });
+}
+
 document.forms[0].onsubmit = async (e) => {
   e.preventDefault();
-  console.log('TESTTT');
+  console.log("TESTTT");
   const data = {
-    email: document.getElementById('email').value,
-    username: document.getElementById('username').value,
-    password: document.getElementById('password').value,
+    email: escapeHtml(document.getElementById("email").value),
+    username: escapeHtml(document.getElementById("username").value),
+    password: escapeHtml(document.getElementById("password").value),
   }; //data from the form
   var sharedInfo = data.email;
-  localStorage.setItem('sharedemail', sharedInfo); //save username to local storage
+  localStorage.setItem("sharedemail", sharedInfo); //save username to local storage
 
   console.log(data);
   const settings = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
 
     body: JSON.stringify(data), //send data as json
   };
   try {
     const fetchResponse = await fetch(
-      'http://localhost:4000/register',
+      "http://localhost:4000/register",
       settings
     );
     const response = await fetchResponse.json(); //response from the server
     console.log(response);
 
-    if (response.status === 'new user') {
+    if (response.status === "new user") {
       //new user created - > redirect
       console.log(sharedInfo);
-      window.location.href = '/login';
+      window.location.href = "/login";
     } else {
       //user with that email already exists
-      const para = document.createElement('p');
+      const para = document.createElement("p");
       const node = document.createTextNode(
-        'An account with that email already exists'
+        "An account with that email already exists"
       );
       para.appendChild(node);
 
-      const element = document.getElementById('box');
-      console.log('element= ' + element);
+      const element = document.getElementById("box");
+      console.log("element= " + element);
       element.appendChild(para);
     }
   } catch (err) {
